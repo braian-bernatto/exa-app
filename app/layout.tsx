c03d2660b7cea1db71cdee8aad073e3ff00e39eb
bernatto_uec <braian5353@gmail.com>
2023-11-28 10:28:53 -0300

    supabase provider updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
+import SupabaseProvider from '@/providers/SupabaseProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Navbar />
-        <main className='pt-20 py-5'>{children}</main>
-        <Footer />
+        <SupabaseProvider>
+          <Navbar />
+          <main className='pt-20 py-5'>{children}</main>
+          <Footer />
+        </SupabaseProvider>
       </body>
     </html>
   )
---
+++ components/Spinner.tsx
@@ -0,0 +1,17 @@
+import { Trophy } from 'lucide-react'
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='z-50 fixed top-0 left-0 w-full h-full flex items-center justify-center'>
+      <span className='absolute w-full h-full bg-white opacity-50'></span>
+      <div className='relative flex items-center justify-center w-12 h-12 bg-white border rounded-full'>
+        <div className='w-12 h-12 rounded-full absolute border-4 border-solid border-gray-200'></div>
+        <Trophy className='text-gray-400' />
+        <div className='w-12 h-12 rounded-full animate-spin absolute border-4 border-solid border-gold border-t-transparent shadow-md'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
